c1ed5f53b9221da7780240d6a172520c68969503
ycozco <ycozco@unsa.edu.pe>
2022-07-20 11:40:33 -0500

    basic templates added

--- test02/ova/ova/views.py
+++ test02/ova/ova/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def index(request, *args, **kwargs):
     return render(request,'index.html',{})
+def user(request, *args, **kwargs):
+    return render(request,'user.html',{})
+def product(request, *args, **kwargs):
+    return render(request,'product.html',{})
+def routine(request, *args, **kwargs):
+    return render(request,'routine.html',{})
+def membership(request, *args, **kwargs):
+    return render(request,'membership.html',{})
+def dance(request, *args, **kwargs):
+    return render(request,'dances.html',{})
